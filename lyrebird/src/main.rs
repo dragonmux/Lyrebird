a52d096cd897a066dd3d01b590aeb4f7d4fbfd33
dragonmux <git@dragonmux.network>
2025-03-25 01:03:20 +0000

    lyrebird: Implemented a stub for the lyrebird interface

--- lyrebird/src/main.rs
+++ lyrebird/src/main.rs
@@ -1,3 +1,31 @@
-fn main()
+#![warn(clippy::unwrap_used)]
+#![allow(clippy::upper_case_acronyms)]
+
+use color_eyre::Result;
+use crossterm::event::{self, Event};
+use ratatui::{DefaultTerminal, Frame};
+
+fn main() -> Result<()>
 {
+	let terminal = ratatui::init();
+	let result = run(terminal);
+	ratatui::restore();
+	result
+}
+
+fn run(mut terminal: DefaultTerminal) -> Result<()>
+{
+	loop
+	{
+		terminal.draw(draw)?;
+		if matches!(event::read()?, Event::Key(_))
+		{
+			break Ok(());
+		}
+	}
+}
+
+fn draw(frame: &mut Frame)
+{
+	frame.render_widget("Lyrebird", frame.area());
 }
