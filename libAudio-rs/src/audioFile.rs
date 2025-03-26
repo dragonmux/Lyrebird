4c3ef741ad85f71c7011c3d9f8ae9303b6d584cf
dragonmux <git@dragonmux.network>
2025-03-26 07:50:42 +0000

    libAudio-rs/audioFile: Re-organised the bindings so things don't get insane trying to put everything in one file

---
+++ libAudio-rs/src/audioFile.rs
@@ -0,0 +1,49 @@
+// SPDX-License-Identifier: BSD-3-Clause
+use std::{ffi::CString, os::{raw::c_void, unix::ffi::OsStrExt}, path::Path, ptr::NonNull};
+
+use crate::bindings::{audioCloseFile, audioOpenR, isAudio};
+
+#[derive(Clone, Debug, Eq, PartialEq)]
+pub struct AudioFile
+{
+	inner: NonNull<c_void>,
+}
+
+impl AudioFile
+{
+	/// Try to open the given file as an audio file
+	pub fn forPathRead(path: &Path) -> Option<AudioFile>
+	{
+		let fileName = path.to_path_buf();
+		let fileName = fileName.as_os_str().as_bytes();
+		let fileName = CString::new(fileName).ok()?;
+
+		let file = unsafe { audioOpenR(fileName.as_ptr()) };
+		Some(AudioFile { inner: NonNull::new(file)? })
+	}
+
+	/// Check if the target file is a valid audio file
+	pub fn isAudio(path: &Path) -> bool
+	{
+		let fileName = path.to_path_buf();
+		let fileName = fileName.as_os_str().as_bytes();
+		let fileName = CString::new(fileName);
+		match fileName
+		{
+			Ok(fileName) => unsafe { isAudio(fileName.as_ptr()) }
+			Err(_) => false
+		}
+	}
+}
+
+impl Drop for AudioFile
+{
+	/// Clean up properly by disposing of the audio file object correctly
+	fn drop(&mut self)
+	{
+		unsafe
+		{
+			audioCloseFile(self.inner.as_ptr());
+		}
+	}
+}
