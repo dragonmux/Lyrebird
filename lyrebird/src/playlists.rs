da512ff2c570a6a1529397b6b7a0d1740d6a58de
dragonmux <git@dragonmux.network>
2025-03-28 19:06:07 +0000

    lyrebird/playlists: Implemented logic for rendering the playlists in a neat panel structure

--- lyrebird/src/playlists.rs
+++ lyrebird/src/playlists.rs
@@ -1,4 +1,8 @@
 // SPDX-License-Identifier: BSD-3-Clause
+use color_eyre::eyre::Result;
+use ratatui::buffer::Buffer;
+use ratatui::layout::{Alignment, Constraint, Layout, Rect};
+use ratatui::widgets::{Block, BorderType, List, ListItem, ListState, Padding, StatefulWidget, Widget};
 use serde::{Deserialize, Serialize};
 
 use crate::{playback::Song, playlist::Playlist};
@@ -8,6 +12,10 @@ pub struct Playlists
 {
 	nowPlaying: Playlist,
 	playlists: Vec<Playlist>,
+	#[serde(skip)]
+	nowPlayingState: ListState,
+	#[serde(skip)]
+	playlistsState: ListState,
 }
 
 impl Playlists
@@ -18,6 +26,62 @@ impl Playlists
 		{
 			nowPlaying: Playlist::new("Now Playing".into()),
 			playlists: Vec::new(),
+			nowPlayingState: ListState::default(),
+			playlistsState: ListState::default(),
 		}
+	}
+}
+
+impl Widget for &mut Playlists
+{
+	fn render(self, area: Rect, buf: &mut Buffer)
+		where Self: Sized
+	{
+		// Split the area up so we can display a listing of all the user's playlists, and what's currently queued
+		let layout = Layout::horizontal([Constraint::Fill(1), Constraint::Fill(1)])
+			.split(area);
+
+		// Render the playlist listing using the internal state object
+		StatefulWidget::render
+		(
+			// Build a list of playlists currently available to the user
+			List::new
+			(
+				self.playlists
+					.iter()
+					.map(|playlist| ListItem::new(playlist.name()))
+			)
+				// Put it in a bordered block for presentation
+				.block
+				(
+					Block::bordered()
+						.title(" Playlists ")
+						.title_alignment(Alignment::Left)
+						.border_type(BorderType::Rounded)
+						.padding(Padding::horizontal(1))
+				),
+			layout[0],
+			buf,
+			&mut self.playlistsState
+		);
+
+		// Render the now playing playlist using the internal state object
+		StatefulWidget::render
+		(
+			// Build a list of all the files in the Now Playing playlist
+			List::new(self.nowPlaying.contents())
+				// Put it in a bordered block for presentation
+				.block
+				(
+					Block::bordered()
+						.title(" Now Playing ")
+						.title_alignment(Alignment::Left)
+						.border_type(BorderType::Rounded)
+						.padding(Padding::horizontal(1))
+				),
+			layout[1],
+			buf,
+			&mut self.nowPlayingState
+		);
 	}
 }
