2f3b47f9a6523f77244bbe74b5877d32b172e2f4
dragonmux <git@dragonmux.network>
2025-03-28 17:12:18 +0000

    lyrebird/playlist: Allow for naming playlists and moving them in and out of files

--- lyrebird/src/playlist.rs
+++ lyrebird/src/playlist.rs
@@ -1,18 +1,28 @@
 // SPDX-License-Identifier: BSD-3-Clause
 use std::path::PathBuf;
 
+use serde::{Deserialize, Serialize};
+
+#[derive(Serialize,Deserialize)]
 pub struct Playlist
 {
+	name: String,
 	entries: Vec<PathBuf>,
 }
 
 impl Playlist
 {
-	fn new() -> Self
+	pub fn new(name: String) -> Self
 	{
 		Self
 		{
+			name,
 			entries: Vec::new(),
 		}
+	}
+
+	pub fn name(&self) -> &str
+	{
+		return self.name.as_str()
 	}
 }
