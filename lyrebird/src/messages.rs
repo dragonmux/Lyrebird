e029fca5807f456c962613371aac5fe2f11e4b6f
dragonmux <git@dragonmux.network>
2026-05-06 17:26:30 +0100

    lyrebird/messages: Defined trait implementations for Tab that allow it to function as a TabBar's value enum and be used with one

--- lyrebird/src/messages.rs
+++ lyrebird/src/messages.rs
@@ -1,8 +1,21 @@
 // SPDX-License-Identifier: BSD-3-Clause
 
+use crate::widgets::tabBar::TabBarEnum;
+
+#[derive(Clone, Copy)]
+pub enum Tab
+{
+	LibraryTree = 1,
+	Artists = 2,
+	Albums = 3,
+	Options = 4,
+	Playlists = 5,
+}
+
+#[derive(Clone)]
 pub enum Message
 {
-	Quit,
+	SwitchTo(Tab),
 	AddToPlaylist,
 	RemoveFromPlaylist,
 	PlayPlaylist,
@@ -11,4 +24,47 @@ pub enum Message
 	StopCurrent,
 	NextTrack,
 	PreviousTrack,
+}
+
+impl Default for Tab
+{
+	fn default() -> Self
+	{
+		Self::LibraryTree
+	}
+}
+
+impl TabBarEnum for Tab
+{
+	type Type = Self;
+
+	fn tabs<'a>() -> &'a[Self]
+	{
+		&[Self::LibraryTree, Self::Artists, Self::Albums, Self::Options, Self::Playlists]
+	}
+
+	fn name(&self) -> &'static str
+	{
+		match self
+		{
+			Self::LibraryTree => "Tree",
+			Self::Artists => "Artists",
+			Self::Albums => "Albums",
+			Self::Options => "Options",
+			Self::Playlists => "Playlist",
+		}
+	}
+
+	fn message_for(&self) -> Message
+	{
+		Message::SwitchTo(self.clone())
+	}
+}
+
+impl Tab
+{
+	const fn value(self) -> usize
+	{
+		self as usize
+	}
 }
