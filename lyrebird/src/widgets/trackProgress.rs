91e8035be7d3834dbf085ff90e181bf17634a914
dragonmux <git@dragonmux.network>
2026-05-09 11:41:15 +0100

    lyrebird/widgets/trackProgress: Removed a now defunct comment from the bottom detailing the old ratatui layout

--- lyrebird/src/widgets/trackProgress.rs
+++ lyrebird/src/widgets/trackProgress.rs
@@ -84,7 +84,6 @@ where
 				]
 			)
 				.width(Length::FillPortion(1))
-				.spacing(5.0)
 				.align_y(Alignment::Center)
 				.padding(Padding {
 					top: 2.0,
@@ -92,6 +91,7 @@ where
 					right: 5.0,
 					left: 5.0,
 				})
+				.spacing(5.0)
 				.into(),
 		];
 
@@ -235,47 +235,3 @@ where
 		Self::new(tabBarWidget)
 	}
 }
-
-// // Build a layout for the footer line
-// let (footerLayout, footerSpacers ) = Layout::horizontal
-// (
-// 	[Constraint::Percentage(50), Constraint::Fill(1), Constraint::Fill(3)]
-// )
-// 	.flex(Flex::SpaceBetween)
-// 	.spacing(1)
-// 	.split_with_spacers(areas[2]);
-
-// // Figure out what strings are to be displayed in the footer
-// let currentlyPlaying = self.currentlyPlaying.as_ref()
-// 	.map_or_else(|| String::from("Nothing playing"), |(song, _)| song.description());
-// let songDuration = self.currentlyPlaying.as_ref()
-// 	.and_then(|(song, _)| song.songDuration())
-// 	.map_or_else
-// 	(
-// 		|| String::from("--:--"), durationAsString
-// 	);
-// let playedDuration = self.currentlyPlaying.as_ref()
-// 	.map_or_else
-// 	(
-// 		|| String::from("--:--"),
-// 		|(song, _)| durationAsString(song.playedDuration())
-// 	);
-// let errorState = self.errorState.as_ref().map_or_else
-// (
-// 	|| String::from("No errors"), Clone::clone
-// );
-
-// // Display the program footer - which song is currently playing, song runtime, and whether errors have occured
-// Line::from_iter([String::from(" "), currentlyPlaying])
-// 	.style(self.footer)
-// 	.render(footerLayout[0], buf);
-// Line::styled(format!("{playedDuration}/{songDuration}"), self.footer)
-// 	.centered()
-// 	.render(footerLayout[1], buf);
-// Line::styled(errorState, self.footer).render(footerLayout[2], buf);
-
-// // Render the spacers for all the components of the footer
-// for spacerRect in footerSpacers.iter()
-// {
-// 	Line::styled("│", self.footer).render(*spacerRect, buf);
-// }
