d893589dbe51153cd6dfc20dbb24742e8f8c10a0
dragonmux <git@dragonmux.network>
2026-05-14 00:29:52 +0100

    lyrebird/playlists: Removed all the old side tracking logic for the playlists

--- lyrebird/src/playlists.rs
+++ lyrebird/src/playlists.rs
@@ -10,16 +10,6 @@ pub struct Playlists
 	nowPlaying: Playlist,
 	#[expect(clippy::struct_field_names, reason = "naming is hard, okay")]
 	playlists: Vec<Playlist>,
-	#[serde(skip)]
-	activeSide: Side,
-}
-
-#[derive(Clone, Copy, Default)]
-enum Side
-{
-	#[default]
-	Playlists,
-	PlaylistContents,
 }
 
 impl Playlists
@@ -30,54 +20,9 @@ impl Playlists
 		{
 			nowPlaying: Playlist::new("Now Playing".into()),
 			playlists: Vec::new(),
-			activeSide: Side::Playlists,
 		}
 	}
 
 	pub fn nowPlaying(&mut self) -> &mut Playlist
 		{ &mut self.nowPlaying }
-
-	const fn moveLeft(&mut self)
-		{ self.activeSide = Side::Playlists; }
-
-	const fn moveRight(&mut self)
-		{ self.activeSide = Side::PlaylistContents; }
-
-	fn moveUp(&mut self)
-	{
-		match self.activeSide
-		{
-			Side::Playlists =>
-			{
-			}
-			Side::PlaylistContents =>
-			{
-			}
-		}
-	}
-
-	fn moveDown(&mut self)
-	{
-		match self.activeSide
-		{
-			Side::Playlists =>
-			{
-			}
-			Side::PlaylistContents =>
-			{
-			}
-		}
-	}
-
-	fn makeSelection(&mut self) -> Operation
-	{
-		match self.activeSide
-		{
-			Side::Playlists => Operation::None,
-			Side::PlaylistContents =>
-			{
-				Operation::None
-			}
-		}
-	}
 }
